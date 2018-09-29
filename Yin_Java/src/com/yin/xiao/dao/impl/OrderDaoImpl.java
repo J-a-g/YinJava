adf24cb86c2b96c004921b6111341b79ee982d1d
JAG <13282221172@163.com>
2018-09-29 21:05:15 +0800

    impl

---
+++ Yin_Java/src/com/yin/xiao/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,28 @@
+package com.yin.xiao.dao.impl;
+
+import java.util.List;
+
+import com.yin.xiao.bean.Order;
+import com.yin.xiao.dao.OrderDao;
+
+public class OrderDaoImpl implements OrderDao{
+
+	@Override
+	public void insertOrder(Order order) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Order> queryOrder(int index, int length) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateOder(Order order) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Yin_Java/src/com/yin/xiao/dao/impl/WeChatUserDaoImpl.java
@@ -0,0 +1,13 @@
+package com.yin.xiao.dao.impl;
+
+import com.yin.xiao.bean.WeChatUser;
+import com.yin.xiao.dao.WeChatUserDao;
+
+public class WeChatUserDaoImpl implements WeChatUserDao{
+
+	@Override
+	public void insertWeChatUser(WeChatUser weChatUser) {
+		// TODO Auto-generated method stub
+		
+	}
+}
